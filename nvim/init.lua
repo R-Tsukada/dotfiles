89d71126c01e283debf7868f99313c32ff6b8f50
ryo-tsukada <62058863+R-Tsukada@users.noreply.github.com>
2025-11-10 14:25:55 +0900

    Initialize Neovim configuration and plugins
    Load various configuration and plugin files for Neovim.

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+dofile(vim.fn.stdpath('config') .. '/core/options.lua')
+dofile(vim.fn.stdpath('config') .. '/core/keymaps.lua')
+dofile(vim.fn.stdpath('config') .. '/plugins/manager.lua')
+dofile(vim.fn.stdpath('config') .. '/plugins/coc.lua')
+dofile(vim.fn.stdpath('config') .. '/plugins/fern.lua')
+dofile(vim.fn.stdpath('config') .. '/plugins/copilot.lua')
+dofile(vim.fn.stdpath('config') .. '/plugins/mcphub.lua')
+dofile(vim.fn.stdpath('config') .. '/plugins/neotest.lua')
+-- vim.cmd('colorscheme gruvbox-material')
+vim.g.tokyonight_style = 'night' -- night, storm, day から選択
+vim.g.tokyonight_enable_italic = 1
+vim.cmd('colorscheme tokyonight')
