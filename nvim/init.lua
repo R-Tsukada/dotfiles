7bf081ad2a5872452df3dfeaa4ced921ff54971f
ryo-tsukada <62058863+R-Tsukada@users.noreply.github.com>
2025-11-11 09:28:18 +0900

    Enable True Color support in init.lua
    Enable True Color support in Neovim configuration.

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,8 @@ dofile(vim.fn.stdpath('config') .. '/plugins/copilot.lua')
 dofile(vim.fn.stdpath('config') .. '/plugins/mcphub.lua')
 dofile(vim.fn.stdpath('config') .. '/plugins/neotest.lua')
 -- vim.cmd('colorscheme gruvbox-material')
+-- True Colorを有効にする (必須)
+vim.opt.termguicolors = true
 vim.g.tokyonight_style = 'night' -- night, storm, day から選択
 vim.g.tokyonight_enable_italic = 1
 vim.cmd('colorscheme tokyonight')
