0adc5e534ecc3111e184518b89651a44382b37e0
ryo-tsukada <62058863+R-Tsukada@users.noreply.github.com>
2026-01-03 16:07:50 +0900

    Add Fern plugin configuration for Neovim

---
+++ nvim/lua/plugins/fern.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'lambdalisue/fern.vim',
+    keys = {
+      { '<Leader>e', '<Cmd>Fern . -drawer<CR>', desc = 'Open Fern drawer' },
+      { '<Leader>E', '<Cmd>Fern . -drawer -reveal=%<CR>', desc = 'Open Fern drawer and reveal current file' },
+    },
+  },
+}
