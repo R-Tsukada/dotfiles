8a1543573f9151fbec6ced95969884cb3c993fdd
ryo-tsukada-LITALICO <ellegarden10314>
2026-02-21 11:25:34 +0900

    整理

--- nvim/lua/plugins/fern.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'lambdalisue/fern.vim',
-    keys = {
-      { '<Leader>e', '<Cmd>Fern . -drawer<CR>', desc = 'Open Fern drawer' },
-      { '<Leader>E', '<Cmd>Fern . -drawer -reveal=%<CR>', desc = 'Open Fern drawer and reveal current file' },
-    },
-  },
-}
--- nvim/plugins/bufferline.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.termguicolors = true
-require("bufferline").setup{}
-
-vim.keymap.set('n', '<Leader>n', ':bnext<CR>')
-vim.keymap.set('n', '<Leader>b', ':bprevious<CR>')
-vim.keymap.set('n', '<Leader>bd', ':bdelete<CR>')
